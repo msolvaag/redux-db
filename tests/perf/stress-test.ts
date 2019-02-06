edd29b4898ea9a2b7260bd89e4aee52d1bda7ed3
magnus <msolvaag@gmail.com>
2019-02-06 14:40:35 +0100

    perf testing

--- tests/perf/stress-test.ts
+++ tests/perf/stress-test.ts
@@ -1,17 +1,19 @@
 // tslint:disable:no-console
 import { range, sum } from "lodash";
+import { MapOf, TableState } from "../../dist/cjs";
 import { session } from "../schema";
 import * as utils from "./utils";
 
-const NUM_POSTS = 1000;
-const NUM_SAMPLES = 100;
+const NUM_POSTS = 10000;
+const NUM_SAMPLES = 1000;
 
 const posts = range(0, NUM_POSTS).map(id => ({ id, body: "data" }));
 const samples: number[] = [];
 
+let state: MapOf<TableState> = {};
 range(0, NUM_SAMPLES).forEach(() => {
     const tstart = utils.timer();
-    session(({ BlogPost }) => BlogPost.insert(posts));
+    state = session(({ BlogPost }) => BlogPost.insert(posts), state);
     const elapsed = utils.timer(tstart);
 
     samples.push(elapsed);
